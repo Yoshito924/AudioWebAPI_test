8a16cdd1c0c91986b03a3971fec16deb7edf9463
Yoshito924 <kimu.murd.cisum@gmail.com>
2023-04-26 01:10:50 +0900

    テスト

---
+++ js/index.js
@@ -0,0 +1,92 @@
+'use strict'
+
+console.log("aa")
+
+// AudioContextの作成
+const audioContext = new AudioContext();
+
+//任意のwavファイルを再生させる関数
+function playAudio(audioFileURL) {
+    // AudioContextの作成
+    const audioContext = new AudioContext();
+
+    // Fetch APIを使用してwavファイルを取得する
+    fetch(audioFileURL)
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => {
+            // 取得したデータをデコードする
+            return audioContext.decodeAudioData(arrayBuffer);
+        })
+        .then(audioBuffer => {
+            // AudioBufferSourceNodeを作成する
+            const source = audioContext.createBufferSource();
+            source.buffer = audioBuffer;
+
+            // 再生する
+            source.connect(audioContext.destination);
+            source.start(0);
+        })
+        .catch(error => console.error(error));
+}
+
+// -------------------------------------------------
+
+let bpm = 150;
+let interval = (60 / bpm) * 1000;
+let nextTickTime = 0;
+let isRunning = false; // メトロノームの状態を追跡する変数を追加
+let requestId; // requestAnimationFrameのIDを格納する変数を追加
+
+let audioFileURL = 'Audio/Snare.wav'
+
+// メトロノームの音を鳴らす関数
+function playBeat(audioFileURL) {
+    // ここでビートを実行（例：音を鳴らす、画面を点滅させるなど）
+    console.log("ビート:", performance.now());
+    playAudio(audioFileURL)
+}
+
+//メトロノームを実行する関数
+function metronome() {
+    const currentTime = performance.now();
+
+    if (currentTime >= nextTickTime) {
+        playBeat(audioFileURL);
+        nextTickTime += interval;
+    }
+
+    // isRunningがtrueの場合のみ、次のフレームでmetronome関数を再度実行
+    if (isRunning) {
+        requestId = requestAnimationFrame(metronome);
+    }
+}
+
+//メトロノームをスタートさせる関数
+function startMetronome() {
+    if (!isRunning) {
+        isRunning = true;
+        nextTickTime = performance.now();
+        requestId = requestAnimationFrame(metronome);
+    }
+}
+
+//メトロノームをストップさせる関数
+function stopMetronome() {
+    if (isRunning) {
+        isRunning = false;
+        cancelAnimationFrame(requestId); // メトロノームを停止するため、requestAnimationFrameをキャンセル
+    }
+}
+
+// スタート・ストップボタン
+function toggleFunction() {
+    if (isRunning) {
+        stopMetronome();
+        isRunning = false;
+        console.log('Stopped!');
+    } else {
+        startMetronome();
+        isRunning = true;
+        console.log('Started!');
+    }
+}
